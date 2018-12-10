0dd8f3a0619b31820230e304e548d8f84a9fcca3
julgargal <juliagarciagallego@gmail.com>
2018-12-10 12:39:31 +0100

    cambio

--- diario/diario_app/models.py
+++ diario/diario_app/models.py
@@ -8,7 +8,7 @@ class Diario(models.Model):
     def __str__(self):
         return self.nombre
 
-class Artista(models.Model):
+class Usuario(models.Model):
     nombre_usuario= models.CharField(max_length=20, primary_key=True)
     passwd=models.CharField(max_length=30)
     email=models.CharField(max_length=50)
