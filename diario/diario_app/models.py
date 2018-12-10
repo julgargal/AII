a6690500452f408e3c12472128dbdf626cc93fe6
julgargal <juliagarciagallego@gmail.com>
2018-12-10 12:38:14 +0100

    artista

--- diario/diario_app/models.py
+++ diario/diario_app/models.py
@@ -8,7 +8,7 @@ class Diario(models.Model):
     def __str__(self):
         return self.nombre
 
-class Usuario(models.Model):
+class Artista(models.Model):
     nombre_usuario= models.CharField(max_length=20, primary_key=True)
     passwd=models.CharField(max_length=30)
     email=models.CharField(max_length=50)
