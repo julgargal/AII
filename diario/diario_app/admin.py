cb3df39a84ad1d751c3effb0cecff6237062ecb3
joaquinperez94 <32302020+joaquinperez94@users.noreply.github.com>
2018-12-10 09:43:28 +0100

    modelo bueno

--- diario/diario_app/admin.py
+++ diario/diario_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from diario_app.models import Diario, Usuario,Tipo,Autor,Noticia
 
 # Register your models here.
+admin.site.register(Diario)
+admin.site.register(Usuario)
+admin.site.register(Tipo)
+admin.site.register(Autor)
+admin.site.register(Noticia)
